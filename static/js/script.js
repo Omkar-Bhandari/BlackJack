84954e8ed7c56a3d4604cf4313a34a7a236f7b82
Omkar-Bhandari <68837275+Omkar-Bhandari@users.noreply.github.com>
2020-12-09 23:23:55 +0530

    Commenting First

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 //Blackjack Code
-
+//All Global Variable
 let  blackjackgame = {
     'you':{'scoreSpan': '#your-score', 'div':'#your-box','score':0},
     'dealer':{'scoreSpan': '#dealers-score', 'div':'#dealers-box','score':0},
@@ -14,53 +14,72 @@ let  blackjackgame = {
     'isHit':false,
 };
 
-const YOU = blackjackgame['you'];
-const DEALER = blackjackgame['dealer'];
+const YOU = blackjackgame['you'];           //To refer the object you from Dict
+const DEALER = blackjackgame['dealer'];     //To refer the object dealer from Dict
 
-const hitSound = new Audio('static/sounds/swish.m4a');
-const winSound = new Audio('static/sounds/cash.mp3');
-const lossSound = new Audio('static/sounds/aww.mp3');
+//Sound Initialization
+const hitSound = new Audio('static/sounds/swish.m4a');      // On Each Card
+const winSound = new Audio('static/sounds/cash.mp3');       //On win
+const lossSound = new Audio('static/sounds/aww.mp3');       //On Losing
 
-document.querySelector("#hit-btn").addEventListener("click",hit);
-document.querySelector("#stand-btn").addEventListener("click",stand);
-document.querySelector("#deal-btn").addEventListener("click",deal);
+// Checking for Button CLick Event
+document.querySelector("#hit-btn").addEventListener("click",hit);           // Hit Button
+document.querySelector("#stand-btn").addEventListener("click",stand);       // Stand Button
+document.querySelector("#deal-btn").addEventListener("click",deal);         // Deal Button
 
 function hit(){
-    if (blackjackgame['isStand']===false){
+    if (blackjackgame['isStand']===false){          // This Button can be clicked if Stand isn't clicked 
         let cardName = randomCard();
         showCard(cardName,YOU);
         updateScore(cardName,YOU);
         showScore(YOU);
     }
-    blackjackgame['isHit']=true;
+    blackjackgame['isHit']=true;                // Hit Button is Clicked signal
 }
 
 function deal(){
-    if (blackjackgame['isturnsOver']===true){
-        let yourImages = document.querySelector('#your-box').querySelectorAll('img');
-        let dealerImages = document.querySelector("#dealers-box").querySelectorAll('img');
+    if (blackjackgame['isturnsOver']===true){                       //Deal can be clicked if all the turns are over 
+        let yourImages = document.querySelector('#your-box').querySelectorAll('img');          //To select all the card images under YOU box
+        let dealerImages = document.querySelector("#dealers-box").querySelectorAll('img');      //To select all the card imaged under DEALER box
         // console.log(yourImages);
         // console.log(dealerImages);
-        for (i=0;i<yourImages.length;i++){
+
+        //Clearing all the images
+        for (i=0;i<yourImages.length;i++){          
             yourImages[i].remove();
         }
         for (i=0;i<dealerImages.length;i++){
             dealerImages[i].remove();
         }
-        resetScore();
+        resetScore();   
     }
 }
 
+function resetScore(){
+    YOU['score'] = 0;           //Score Reset to 0
+    DEALER['score'] = 0;        //Score reset to 0
+    document.querySelector(YOU['scoreSpan']).textContent = YOU['score'];        //Resetting on Front-end
+    document.querySelector(YOU['scoreSpan']).style.color = 'white';             //Color Reset on Frond-end
+    document.querySelector(DEALER['scoreSpan']).textContent = DEALER['score'];  //Resetting on Front-end
+    document.querySelector(DEALER['scoreSpan']).style.color = 'white';          //Color Reset on Frond-end
+    document.querySelector("#result").textContent = "Let's Play";
+    document.querySelector("#result").style.color = "black";
+    blackjackgame['isResult']=false;                // Resetting result shown signal to False
+    blackjackgame['isStand']=false;                 // Resetting Stand button click to False
+    blackjackgame['isturnsOver']=false;             // Resetting All turns over to False
+    blackjackgame['isHit']=false;                   // Resetting Hit button click to False
+}
+
 function randomCard(){
-    let randomIndex = Math.floor(Math.random()*13);
+    let randomIndex = Math.floor(Math.random()*13);         // This selects a random no between 0 to 12 which is index 
     // console.log(randomIndex);
     // console.log(blackjackgame['cards'][randomIndex]);
-    return blackjackgame['cards'][randomIndex];
+    return blackjackgame['cards'][randomIndex];             //This selects card from cards dict from global vaar
 }
 
 function showCard(card ,activePlayer){
-    if (activePlayer['score'] <= 21){
-        let cardImage = document.createElement('img');
+    if (activePlayer['score'] <= 21){                   // To stop Showing card if total is less than 21
+        let cardImage = document.createElement('img');      // To create Image element for F-E for Cards
         cardImage.src= 'static/images/'+ card +'.png';
         document.querySelector(activePlayer['div']).appendChild(cardImage);
         hitSound.play();
@@ -91,21 +110,6 @@ function showScore(activePlayer){
     }
 }
 
-function resetScore(){
-    YOU['score'] = 0;
-    DEALER['score'] = 0;
-    document.querySelector(YOU['scoreSpan']).textContent = YOU['score'];
-    document.querySelector(YOU['scoreSpan']).style.color = 'white';
-    document.querySelector(DEALER['scoreSpan']).textContent = DEALER['score'];
-    document.querySelector(DEALER['scoreSpan']).style.color = 'white';
-    document.querySelector("#result").textContent = "Let's Play";
-    document.querySelector("#result").style.color = "black";
-    blackjackgame['isResult']=false;
-    blackjackgame['isStand']=false;
-    blackjackgame['isturnsOver']=false;
-    blackjackgame['isHit']=false;
-
-}
 
 function sleep(ms){
     return new Promise(resolve => setTimeout(resolve,ms));
